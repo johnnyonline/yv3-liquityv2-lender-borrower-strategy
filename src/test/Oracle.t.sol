c5a8c32e230f01700585aea9c613d61d6a77acb8
johnnyonline <johnnyonline.eth@gmail.com>
2025-07-01 19:26:48 -0300

    feat: add apr oracle

--- src/test/Oracle.t.sol
+++ src/test/Oracle.t.sol
@@ -11,46 +11,35 @@ contract OracleTest is Setup {
 
     function setUp() public override {
         super.setUp();
-        oracle = new StrategyAprOracle();
+        oracle = new StrategyAprOracle(management);
     }
 
     function checkOracle(address _strategy, uint256 _delta) public {
+        // Strategist makes initial deposit and opens a trove
+        strategistDepositAndOpenTrove(true);
+
         // Check set up
-        // TODO: Add checks for the setup
+        assertEq(oracle.governance(), management);
 
         uint256 currentApr = oracle.aprAfterDebtChange(_strategy, 0);
+        console2.log("currentApr", currentApr);
 
         // Should be greater than 0 but likely less than 100%
         assertGt(currentApr, 0, "ZERO");
         assertLt(currentApr, 1e18, "+100%");
 
-        // TODO: Uncomment to test the apr goes up and down based on debt changes
-        /**
-         * uint256 negativeDebtChangeApr = oracle.aprAfterDebtChange(_strategy, -int256(_delta));
-         *
-         *     // The apr should go up if deposits go down
-         *     assertLt(currentApr, negativeDebtChangeApr, "negative change");
-         *
-         *     uint256 positiveDebtChangeApr = oracle.aprAfterDebtChange(_strategy, int256(_delta));
-         *
-         *     assertGt(currentApr, positiveDebtChangeApr, "positive change");
-         */
+        uint256 negativeDebtChangeApr = oracle.aprAfterDebtChange(_strategy, -int256(_delta));
 
-        // TODO: Uncomment if there are setter functions to test.
-        /**
-         * vm.expectRevert("!governance");
-         *     vm.prank(user);
-         *     oracle.setterFunction(setterVariable);
-         *
-         *     vm.prank(management);
-         *     oracle.setterFunction(setterVariable);
-         *
-         *     assertEq(oracle.setterVariable(), setterVariable);
-         */
+        // The apr should go up if deposits go down
+        assertLt(currentApr, negativeDebtChangeApr, "negative change");
+
+        uint256 positiveDebtChangeApr = oracle.aprAfterDebtChange(_strategy, int256(_delta));
+
+        assertGt(currentApr, positiveDebtChangeApr, "positive change");
     }
 
     function test_oracle(uint256 _amount, uint16 _percentChange) public {
-        vm.assume(_amount > minFuzzAmount && _amount < maxFuzzAmount);
+        vm.assume(_amount > minFuzzAmount * 1e12 && _amount < maxFuzzAmount);
         _percentChange = uint16(bound(uint256(_percentChange), 10, MAX_BPS));
 
         mintAndDepositIntoStrategy(strategy, user, _amount);
